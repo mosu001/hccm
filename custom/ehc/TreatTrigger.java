5992f368cab0218afa8a3e7384d7b0fb2205cf63
Jack Collinson <phantekx@gmail.com>
2020-08-23 16:00:18 +1200

    Updates to EHC model in custom
    Bunch of updates to this model. It's fairly close to being up and
    running, there are a few issues to hunt down with triggers and then
    state assignments and log events need to be set up but the structure is
    there and it begins running at least.
    
    Changes:
    
    - Changed stats objects to log events
    - Set initial entity states
    - Set initial and default state lists
    - Converted model to have walkup patients shared between doctors
      (Doctor1 on walkup, Doctor2 on scheduled and walkup if free)
    - Added a triage start trigger and logic
    - Added a triage end trigger and logic
    - Added a test start trigger
    - Added a treat start trigger
    - Added include file for triggers and modified autoload.cfg
    - Added basenames for entities and entity graphics
    - Changed needstestdist from discrete to boolean since is wasn't working
      properly. Will probably change this back later
    
    To-do:
    
    - Fix triage end trigger so that nurse goes back to WaitToTriage and
      Walkup patient moves on to either MoveToTest or MoveToTreat. This is
      tricky because you can't have conditionals in the NextActivityList
      field, and the trigger logic has errors with passing entities to the
      WaitActivity or ProcessActivity to debug
    - Fix treat start trigger
    - Set up state assignments and log events

---
+++ custom/ehc/TreatTrigger.java
@@ -0,0 +1,111 @@
+package ehc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import hccm.controlunits.ControlUnit;
+import hccm.controlunits.Trigger;
+import hccm.controlunits.ControlUnit.Request;
+import hccm.controlunits.ControlUnit.RequestUtils;
+import hccm.entities.ActiveEntity;
+import hccm.entities.Entity;
+
+/**
+ * 
+ * @author Jack Collinson
+ * @version 0.0.1
+ * @since 0.0.1
+ * 
+ */
+public class TreatTrigger extends Trigger {
+
+	/**
+	 * Overrides parent class, executes the logic of the triage queue
+	 * @param ent, an ActiveEntity object
+	 * @param simTime, a double, the sim time
+	 */
+	@Override
+	public void executeLogic(List<ActiveEntity> ents, double simTime) {
+		ControlUnit cu = getControlUnit();
+		// If there are any requests, then sort them by time
+		List<Request> requests = cu.getRequestList();
+		if (requests.size() > 1) {
+			Collections.sort(requests, RequestUtils::compareWhenRequested);
+			Request creq = null, sreq = null;
+			
+			/*
+			 * There are three possible actions this trigger can controll:
+			 * 1. Doctor 2 sees scheduled patient (if there are requests from a scheduled patient and doctor2)
+			 * 2. Doctor 1 sees walkup patient (if there are requests from a walk up patient and doctor1)
+			 * 3. Doctor 2 sees walkup patient (if there are requests from a walk up patient and doctor2)
+			 * 
+			 * We need to check in this order, because we only want doctor 2 to see walkup patients if doctor1
+			 * is unavailable, otherwise we will waste doctor 1s time. Since doctor 2 is more flexible, it is 
+			 * better to have him available and assign doctor 1 first.
+			 */
+			
+			// Look for a scheduled patient and doctor 2 request
+	        for (Request r: requests) {
+	        	System.out.println(r);
+	        	if ( (creq == null) && (r.getRequester().getName().startsWith("ScheduledPatient")) )
+	        		creq = r;
+	        	if ( (sreq == null) && (r.getRequester().getName().startsWith("Doctor2")) )
+	        		sreq = r;
+	        	if ( (creq != null) & (sreq != null) )
+	        		// Remove doctor 2 walkup request to prevent doubling up
+	        		for (Request req: requests) {
+	        			if ( (sreq == null) && (req.getRequester().getName().startsWith("Doctor2")));
+	        		}
+	        		
+	        		break;
+	        }
+	        
+	        // If previous not found
+	        if ( (creq == null) || (sreq == null) ) {
+				// Look for a walk up patient and doctor 1 request
+	        	creq = null;
+	        	sreq = null;
+		        for (Request r: requests) {
+		        	System.out.println(r);
+		        	if ( (creq == null) && (r.getRequester().getName().startsWith("WalkUpPatient")) )
+		        		creq = r;
+		        	if ( (sreq == null) && (r.getRequester().getName().startsWith("Doctor1")) )
+		        		sreq = r;
+		        	if ( (creq != null) & (sreq != null) )
+		        		break;
+		        }
+	        }
+	        
+	     // If previous not found
+	        if ( (creq != null) & (sreq != null) ) {
+	        	creq = null;
+	        	sreq = null;
+				// Loop over the requests until one customer request and one server request are found
+		        for (Request r: requests) {
+		        	System.out.println(r);
+		        	if ( (creq == null) && (r.getRequester().getName().startsWith("WalkUpPatient")) )
+		        		creq = r;
+		        	if ( (sreq == null) && (r.getRequester().getName().startsWith("Doctor2")) )
+		        		sreq = r;
+		        	if ( (creq != null) & (sreq != null) )
+		        		break;
+		        }
+	        }
+	        
+	        // Both a customer and a server have been found waiting
+        	if ( (creq != null) & (sreq != null) ) {
+		        ActiveEntity cust = creq.getRequester(), serv = sreq.getRequester();
+		        ArrayList<ActiveEntity> participants = new ArrayList<ActiveEntity>(Arrays.asList(cust, serv));
+		        requests.remove(creq);
+		        requests.remove(sreq);
+		        creq.getWaiting().finish(cust.asList());
+		        sreq.getWaiting().finish(serv.asList());
+		        assert(creq.getRequested().getName().equals(sreq.getRequested().getName()));
+		        creq.getRequested().start(participants);
+        	}
+		}
+	}
+
+}
