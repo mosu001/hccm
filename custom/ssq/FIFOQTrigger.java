095d11dd086c0aff54c373cbd161996deb7848a9
Mike <mjosu001@gmail.com>
2020-09-03 22:49:16 +1200

    Fixed SSQ models and FIFOQTrigger.java to only need requests from customers!

--- custom/ssq/FIFOQTrigger.java
+++ custom/ssq/FIFOQTrigger.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import com.jaamsim.basicsim.JaamSimModel;
+
+import hccm.activities.WaitActivity;
 import hccm.controlunits.ControlUnit;
 import hccm.controlunits.Trigger;
 import hccm.controlunits.ControlUnit.Request;
@@ -31,29 +34,40 @@ public class FIFOQTrigger extends Trigger {
 		ControlUnit cu = getControlUnit();
 		// If there are any requests, then sort them by time
 		List<Request> requests = cu.getRequestList();
-		if (requests.size() > 1) {
+		if (requests.size() > 0) {
 			Collections.sort(requests, RequestUtils::compareWhenRequested);
-			// Loop over the requests until one customer request and one server request are found
-			Request creq = null, sreq = null;
+			// Get the first customer request
+			Request creq = null;
 	        for (Request r: requests) {
 	        	System.out.println(r);
-	        	if ( (creq == null) && (r.getRequester().getName().startsWith("Customer")) )
+	        	if ( (creq == null) && (r.getRequester().getName().startsWith("Customer")) ) {
 	        		creq = r;
-	        	if ( (sreq == null) && (r.getRequester().getName().startsWith("Server")) )
-	        		sreq = r;
-	        	if ( (creq != null) & (sreq != null) )
 	        		break;
+	        	}
 	        }
-	        // Both a customer and a server have been found waiting
-        	if ( (creq != null) & (sreq != null) ) {
-		        ActiveEntity cust = creq.getRequester(), serv = sreq.getRequester();
-		        ArrayList<ActiveEntity> participants = new ArrayList<ActiveEntity>(Arrays.asList(cust, serv));
-		        requests.remove(creq);
-		        requests.remove(sreq);
-		        creq.getWaiting().finish(cust.asList());
-		        sreq.getWaiting().finish(serv.asList());
-		        assert(creq.getRequested().getName().equals(sreq.getRequested().getName()));
-		        creq.getRequested().start(participants);
+	        
+	        // If a customer request has been found...
+        	if (creq != null) {
+		        ActiveEntity cust = creq.getRequester(), serv = null;
+        		// ... check for a waiting server
+        		JaamSimModel model = cu.getJaamSimModel();
+        		WaitActivity wait = (WaitActivity)model.getEntity("WaitForCustomer");
+        		List<ActiveEntity> waiting = wait.getEntities();
+        		if (waiting.size() > 0) {
+        			// The server is waiting, pick them
+        			// TODO: if more than one server is waiting, then how do you pick, e.g.,
+        			// the one waiting longest. Might need to associate a time with
+        			// activities an entity is currently participating in (currently can
+        			// only be one but this should be extended in the future)
+        			serv = waiting.get(0);
+        		}
+        		if (serv != null) {
+			        ArrayList<ActiveEntity> participants = new ArrayList<ActiveEntity>(Arrays.asList(cust, serv));
+			        requests.remove(creq);
+			        creq.getWaiting().finish(cust.asList());
+			        wait.finish(serv.asList());
+			        creq.getRequested().start(participants);
+        		}
         	}
 		}
 	}
